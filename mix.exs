ba6a1bc0473e754b5363f974da40cac49d03d752
Jonathan Clem <jonathan@jclem.net>
2016-12-19 17:04:09 -0500

    Add docs/package info to mix.exs

--- mix.exs
+++ mix.exs
@@ -1,9 +1,17 @@
 defmodule CanQL.Mixfile do
   use Mix.Project
 
+  @version "1.0.0"
+  @github_url "https://github.com/usecanvas/canql"
+
   def project do
     [app: :canql,
-     version: "0.1.0",
+     description: "A natural-ish language query parser",
+     package: package,
+     docs: docs,
+     source_url: @github_url,
+     homepage_url: @github_url,
+     version: @version,
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -28,11 +36,25 @@ defmodule CanQL.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:dialyxir, "~> 0.4", only: [:dev, :test], runtime: false},
+    [{:ex_doc, "> 0.0.0", only: [:dev]},
+     {:dialyxir, "~> 0.4", only: [:dev, :test], runtime: false},
      {:credo, "~> 0.5", only: [:dev, :test]}]
   end
 
   defp aliases do
     [ci: ["test", "credo --strict", "dialyzer"]]
+  end
+
+  defp package do
+    [maintainers: ["Jonathan Clem <jonathan@usecanvas.com>"],
+     licenses: ["MIT"],
+     links: %{GitHub: @github_url},
+     files: ~w(lib mix.exs LICENSE.md README.md)]
+  end
+
+  defp docs do
+    [source_ref: "v#{@version}",
+     main: "readme",
+     extras: ~w(README.md LICENSE.md)]
   end
 end
